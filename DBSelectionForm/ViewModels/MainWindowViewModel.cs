c7d4abe9a4b78374e310531498675f10ff339fb4
SokurenkoED <SokurenkoED@yandex.ru>
2021-10-31 13:14:58 +0300

    Привязали заголовок программы

---
+++ DBSelectionForm/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using DBSelectionForm.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBSelectionForm.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+        #region Заголовок окна
+
+        private string _Title = "Выборка из базы данных";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+
+    }
+}
