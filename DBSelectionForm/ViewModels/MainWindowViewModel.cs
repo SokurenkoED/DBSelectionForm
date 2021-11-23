d6e3897bd18910c28f79b57399b5e0de417ddd88
SokurenkoED <SokurenkoED@yandex.ru>
2021-11-23 18:46:59 +0300

    добавил обработку сигналов nERn

--- DBSelectionForm/ViewModels/MainWindowViewModel.cs
+++ DBSelectionForm/ViewModels/MainWindowViewModel.cs
@@ -2,21 +2,14 @@
 using DBSelectionForm.Models;
 using DBSelectionForm.Services;
 using DBSelectionForm.ViewModels.Base;
-using Microsoft.Win32;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.ComponentModel;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Threading;
 
 namespace DBSelectionForm.ViewModels
 {
