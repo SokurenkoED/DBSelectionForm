8591580d876c68a983dd2640e30dcffac70d5c6f
SokurenkoED <SokurenkoED@yandex.ru>
2022-02-21 13:39:28 +0300

    Сделал выделениие цветом строки в datagrid , если несовпадают значения new value и old value

---
+++ DBSelectionForm/Converters/ValuesToColorConverters.cs
@@ -0,0 +1,37 @@
+﻿using DBSelectionForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace DBSelectionForm.Converters
+{
+    class ValuesToColorConverters : IValueConverter
+    {
+
+        public object Convert(object values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var SM = values as SignalModel;
+            if (SM == null)
+            {
+                return new SolidColorBrush(Colors.White);
+            }
+            if (SM.NewValue.ToString() != SM.OldValue.ToString())
+            {
+                return new SolidColorBrush(Colors.Pink);
+            }
+
+            return new SolidColorBrush(Colors.White);
+        }
+
+
+        public object ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+    }
+}
