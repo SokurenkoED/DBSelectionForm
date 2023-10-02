80e2b835727afb32896853cff8fc3ee6d1ea8117
Евгений Сокуренко <sokurenkoed@yandex.ru>
2023-10-02 12:55:43 +0300

    доработал

--- DBSelectionForm/Services/GetListFromDB.cs
+++ DBSelectionForm/Services/GetListFromDB.cs
@@ -1276,7 +1276,7 @@ namespace DBSelectionForm.Services
                 MessageBox.Show($"Список создан");
                 return FinalSignalsList;
             }
-            catch (DivideByZeroException ex)
+            catch (Exception ex)
             {
                 _TextInformationFromListDB.Clear();
                 _TextInformationFromListDB.Add($"{ex.Message}");
